891cd02c8815a746395b6039c32ae00e448e442a
liang cao <caoliang0914@gmail.com>
2019-05-26 15:32:24 +0800

    Initial commit

---
+++ src/main/java/com/sse/ftpTool/FtpToolApplication.java
@@ -0,0 +1,13 @@
+package com.sse.ftpTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtpToolApplication.class, args);
+	}
+
+}
